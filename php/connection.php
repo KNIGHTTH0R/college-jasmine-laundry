b9933456ef732eca2c12f113de7ea74f8b7202e2
firmannf <firmannizammudin@gmail.com>
2017-01-01 00:22:16 +0700

    Initial Commit

--- php/connection.php
+++ php/connection.php
@@ -1,8 +1,14 @@
 <?php
-    $host = "localhost";
-    $user = "root";
-    $pass = "";
-    $database = "db_laundry_jasmine";
+    // Local
+    // $host = "localhost";
+    // $user = "root";
+    // $pass = "";
+    // $database = "db_laundry_jasmine";
+
+    $host = "ap-cdbr-azure-southeast-b.cloudapp.net";
+    $user = "b11e7dbec83707";
+    $pass = "efd5017c";
+    $database = "db_jasmine_laundry";
     
     $connection = mysqli_connect($host, $user, $pass, $database);
     if (mysqli_connect_errno()) {
