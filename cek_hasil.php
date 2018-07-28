5ff423522551eb50dbafe33ba126af348c215225
Unikom Codelabs <firmannizammudin@gmail.com>
2018-07-28 19:37:51 +0700

    Initial

--- cek_hasil.php
+++ cek_hasil.php
@@ -31,25 +31,25 @@
                         <div class="card">
                             <div class="header">
                                 <?php
-                                    $statement = $connection->prepare("SELECT n.nota_id, n.nota_status
-                                                                    FROM transaksi n
-                                                                    INNER JOIN pelanggan p ON n.pelanggan_id = p.pelanggan_id
-                                                                    INNER JOIN agen a ON p.agen_id = a.agen_id
-                                                                    WHERE n.nota_id = ? AND n.nota_deleted = 'false'");
-                                    $statement->bind_param('s', $no_nota);
-                                    $statement->execute();
-                                    $result = $statement->get_result();
-                                    $row = $result->fetch_assoc();
-                                    if($row['nota_status'] == "Belum Bayar"){
+                                    $strQuery = "SELECT n.nota_id, n.nota_status
+                                                    FROM transaksi n
+                                                    INNER JOIN pelanggan p ON n.pelanggan_id = p.pelanggan_id
+                                                    INNER JOIN agen a ON p.agen_id = a.agen_id
+                                                    WHERE n.nota_id = $no_nota AND n.nota_deleted = 'false'";
+                                    $query = mysqli_query($connection, $strQuery);
+                                    if($query) {
+                                        $result = mysqli_fetch_array($query, MYSQLI_ASSOC);
+                                        if($result['nota_status'] == "Belum Bayar"){
                                 ?>
                                         <a href="#" class="btn btn-danger btn-fill pull-right" style="pointer-events: none;cursor: default;">Belum Bayar</a>
-                                <?php
-                                    } else if($row['nota_status'] == "Sudah Bayar"){
-                                ?>
-                                        <a href="#" class="btn btn-success btn-fill pull-right" style="pointer-events: none;cursor: default;">Sudah Bayar</a>
-                                <?php
+                                        <?php
+                                        } else if($result['nota_status'] == "Sudah Bayar"){
+                                        ?>
+                                            <a href="#" class="btn btn-success btn-fill pull-right" style="pointer-events: none;cursor: default;">Sudah Bayar</a>
+                                        <?php
+                                        }
                                     }
-                                ?>
+                                        ?>
                                 <h4 class="title">Data Transaksi</h4>
                                 <p class="category">Detail data transaksi</p>
                             </div>
@@ -65,33 +65,35 @@
                                     </thead>
                                     <tbody>
                                         <?php
-                                            $statement = $connection->prepare("SELECT n.nota_id, p.pelanggan_nama, n.nota_tgl_masuk, n.nota_tgl_selesai, n.nota_status
-                                                                                FROM transaksi n
-                                                                                INNER JOIN pelanggan p ON n.pelanggan_id = p.pelanggan_id
-                                                                                INNER JOIN agen a ON p.agen_id = a.agen_id
-                                                                                WHERE n.nota_id = ? AND n.nota_deleted = 'false'");
-                                                    $statement->bind_param('s', $no_nota);
-                                                    $statement->execute();
-                                                    $result = $statement->get_result();
-                                                    while($row = $result->fetch_assoc()){
-                                                        echo "<tr>";
-                                                        echo "<td>$row[nota_id]</td>";
-                                                        echo "<td>$row[pelanggan_nama]</td>";
-                                                        echo "<td>$row[nota_tgl_masuk]</td>";
-                                                        echo "<td>$row[nota_tgl_selesai]</td>";
-                                                        $strSubQuery = "SELECT njc.nota_jeniscucian_id, jc.jeniscucian_nama, njc.nota_jeniscucian_jumlah, njc.nota_jeniscucian_subtotal
+                                                    $strQuery = "SELECT n.nota_id, p.pelanggan_nama, n.nota_tgl_masuk, n.nota_tgl_selesai, n.nota_status
+                                                    FROM transaksi n
+                                                    INNER JOIN pelanggan p ON n.pelanggan_id = p.pelanggan_id
+                                                    INNER JOIN agen a ON p.agen_id = a.agen_id
+                                                    WHERE n.nota_id = $no_nota AND n.nota_deleted = 'false'";
+                                                    $query = mysqli_query($connection, $strQuery);
+                                                    if($query) {
+                                                        $i = 0;
+                                                        while($result = mysqli_fetch_assoc($query)){
+                                                            echo "<tr>";
+                                                            echo "<td>$result[nota_id]</td>";
+                                                            echo "<td>$result[pelanggan_nama]</td>";
+                                                            echo "<td>$result[nota_tgl_masuk]</td>";
+                                                            echo "<td>$result[nota_tgl_selesai]</td>";
+                                                            $strSubQuery = "SELECT njc.nota_jeniscucian_id, jc.jeniscucian_nama, njc.nota_jeniscucian_jumlah, njc.nota_jeniscucian_subtotal
                                                             FROM nota_jeniscucian njc
                                                             INNER JOIN jeniscucian jc ON njc.jeniscucian_id = jc.jeniscucian_id
                                                             INNER JOIN transaksi n ON njc.nota_id = n.nota_id
-                                                            WHERE njc.nota_id = $row[nota_id] AND n.nota_deleted = 'false'";
-                                                        $subQuery = mysqli_query($connection, $strSubQuery);
-                                                        $total = 0;
-                                                        while($subResult = mysqli_fetch_assoc($subQuery)){
-                                                            $total += $subResult['nota_jeniscucian_subtotal'];
+                                                            WHERE njc.nota_id = $result[nota_id] AND n.nota_deleted = 'false'";
+                                                            $subQuery = mysqli_query($connection, $strSubQuery);
+                                                            $total = 0;
+                                                            while($subResult = mysqli_fetch_assoc($subQuery)){
+                                                                $total += $subResult['nota_jeniscucian_subtotal'];
+                                                            }
+                                                            echo "<td>$total</td>";
+                                                            echo "<td>$result[nota_status]</td>";
+                                                            echo "</tr>";
+                                                            $i++;
                                                         }
-                                                        echo "<td>$total</td>";
-                                                        echo "<td>$row[nota_status]</td>";
-                                                        echo "</tr>";
                                                     }
                                                 ?>
                                     </tbody>
@@ -114,22 +116,24 @@
                                     </thead>
                                     <tbody>
                                         <?php
-                                            $statement = $connection->prepare("SELECT njc.nota_jeniscucian_id, jc.jeniscucian_nama, njc.nota_jeniscucian_jumlah, njc.nota_jeniscucian_subtotal
-                                                                                FROM nota_jeniscucian njc
-                                                                                INNER JOIN jeniscucian jc ON njc.jeniscucian_id = jc.jeniscucian_id
-                                                                                INNER JOIN transaksi n ON njc.nota_id = n.nota_id
-                                                                                WHERE njc.nota_id = ? AND n.nota_deleted = 'false'");
-                                            $statement->bind_param('s', $no_nota);
-                                            $statement->execute();
-                                            $result = $statement->get_result();
-                                            while($row = mysqli_fetch_assoc($result)){
-                                                echo "<tr>";
-                                                echo "<td>$row[jeniscucian_nama]</td>";
-                                                echo "<td>$row[nota_jeniscucian_jumlah]</td>";
-                                                echo "<td>$row[nota_jeniscucian_subtotal]</td>";
-                                                echo "</tr>";
-                                            }
-                                        ?>
+                                                    $strQuery = "SELECT njc.nota_jeniscucian_id, jc.jeniscucian_nama, njc.nota_jeniscucian_jumlah, njc.nota_jeniscucian_subtotal
+                                                    FROM nota_jeniscucian njc
+                                                    INNER JOIN jeniscucian jc ON njc.jeniscucian_id = jc.jeniscucian_id
+                                                    INNER JOIN transaksi n ON njc.nota_id = n.nota_id
+                                                    WHERE njc.nota_id = $no_nota AND n.nota_deleted = 'false'";
+                                                    $query = mysqli_query($connection, $strQuery);
+                                                    if ($query) {
+                                                        $i = 0;
+                                                        while($result = mysqli_fetch_assoc($query)){
+                                                            echo "<tr>";
+                                                            echo "<td>$result[jeniscucian_nama]</td>";
+                                                            echo "<td>$result[nota_jeniscucian_jumlah]</td>";
+                                                            echo "<td>$result[nota_jeniscucian_subtotal]</td>";
+                                                            echo "</tr>";
+                                                            $i++;
+                                                        }
+                                                    }
+                                                ?>
                                     </tbody>
                                 </table>
                             </div>
